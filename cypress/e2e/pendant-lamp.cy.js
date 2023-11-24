bae6fb4b74f664c5505ccf1c6854372394a27f0e
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2023-11-24 13:04:13 +0100

    Initial commit

---
+++ cypress/e2e/pendant-lamp.cy.js
@@ -0,0 +1,22 @@
+describe('Check for new results in outlets', () => {
+  const baseUrl = process.env.BASE_URL_OUTLET_A;
+  const locations =
+    'locationName=lil-b&locationName=osl-210b&locationName=osl-215b&locationName=osl-217b&locationName=osl-221b&locationName=osl-228b&locationName=osl-231b&locationName=osl-b';
+  const room = 'room=belysning';
+  const category = 'category=pendler';
+  const family = 'family=acorn';
+
+  it('Pendant lamp', () => {
+    cy.visit(`${baseUrl}?${locations}&${room}&${category}&${family}`);
+
+    // Handle cookie consent banner if present
+    cy.get('#coiOverlay').then((val) => {
+      if (val) {
+        cy.get('#declineButton').click();
+      }
+    });
+
+    // Empty results
+    cy.contains('Søket ditt ga ingen resultater');
+  });
+});
